3f1bb6da9e2a02c5315d676a15adfe34adcd1d18
Lucas Conte <144933020+lucascontee@users.noreply.github.com>
2023-12-05 14:18:40 -0300

    Conclusão do desafio 01

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-let nome = "Lucas"
-console.log(`Olá, mundo. Me chamo ${nome}`)
+var nome = "Lucas"
+var xp = 1000
+
+if(xp <= 1000){
+    var elo = 'Ferro'
+} else if(xp <= 2000){
+    var elo = 'Bronze'
+} else if(xp <= 5000){
+    var elo = 'Prata'
+} else if(xp <= 7000){
+    var elo = 'Ouro'
+} else if(xp <= 8000){
+    var elo = 'Platina'
+}else if(xp <= 9000){
+    var elo = 'Ascendente'
+} else if(xp <=10000){
+    var elo = 'Imortal'
+}else{
+    var elo = 'Radiante'
+}
+
+console.log(`O Herói de nome ${nome} está no nível de ${elo}`)
+
